8dbb0f2b943ccd2a462d9d37dd56689003a3c90d
Brad Morris <brad@bradleymorris.co.uk>
2024-07-09 19:45:17 +0000

    Fix issue with MRR/ARR scheduling

--- includes/Bootstrap.php
+++ includes/Bootstrap.php
@@ -24,6 +24,28 @@ class Bootstrap extends Abstracts\AbstractSingleton {
 
 		add_filter( 'woocommerce_admin_rest_controllers', array( $this, 'register_rest_controllers' ) );
 		add_filter( 'woocommerce_data_stores', array( $this, 'add_data_stores' ) );
+
+		$this->schedule();
+
+		add_action( 'sos_analytics_generate_snapshot_data', array( Admin\API\Reports\RecurringRevenue\Stats\DataStore::class, 'calculate_current_data' ) );
+	}
+
+	/**
+	 * Creates Action Scheduler tasks for background operations
+	 *
+	 * @return void 
+	 */
+	private function schedule() {
+		if ( false === as_has_scheduled_action( 'sos_analytics_generate_snapshot_data' ) ) {
+			as_schedule_recurring_action(
+				strtotime( 'tomorrow' ),
+				apply_filters( 'sos_analytics_generate_snapshot_data_interval', DAY_IN_SECONDS / 4 ),
+				'sos_analytics_generate_snapshot_data',
+				array(),
+				'',
+				true
+			);
+		}
 	}
 
 	/**
